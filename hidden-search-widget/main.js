8dccb73e777438c191a71654d335466c0cff0a51
Can Guler <canmuhammetguler@gmail.com>
2021-01-03 22:12:44 -0600

    hidden search widget

---
+++ hidden-search-widget/main.js
@@ -0,0 +1,7 @@
+const search = document.querySelector('.search');
+const btn1 = document.querySelector('.btn1');
+const input = document.querySelector('.input');
+
+btn1.addEventListener('click', function(){
+    search.classList.toggle('active')
+})
